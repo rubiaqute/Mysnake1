b1e3ff13122ff000d23e71e0828c18d78aaf3ee5
rubiaqute <rubiaqute@gmail.com>
2021-03-15 21:47:57 +0300

    Создание класса Point

---
+++ Mysnake1/Mysnake1/Point.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mysnake1
+{
+	class Point
+	{
+		public int x;
+		public int y;
+		public char sym;
+
+		public void Draw()
+		{
+			Console.SetCursorPosition(x, y);
+			Console.Write(sym);
+		}
+	}
+}
