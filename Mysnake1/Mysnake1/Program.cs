bb4a6b26c8639c5c2e36ff7789d6b6abff007ed5
rubiaqute <rubiaqute@gmail.com>
2021-03-16 22:43:14 +0300

    Класс List

--- Mysnake1/Mysnake1/Program.cs
+++ Mysnake1/Mysnake1/Program.cs
@@ -1,68 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Mysnake1
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-			
-			int x = 1;
-			Func1(x);
-			Console.WriteLine("Call Func1. x = " + x);
-
-			x = 1;
-			Func2(x);
-			Console.WriteLine("Call Func2. x = " + x);
-
-			x = 1;
-			Func3(x);
-			Console.WriteLine("Call Func3. x = " + x);
+	class Program
+	{
+		static void Main(string[] args)
+		{
 
 			Point p1 = new Point(1, 3, '*');
-			Move(p1, 10, 10);
-			Console.WriteLine("Call Move. p1.x = " + p1.x + ", p1.y = " + p1.y);
+			p1.Draw();
 
 			Point p2 = new Point(4, 5, '#');
-			p1 = p2;
-			p2.x = 8;
-			p2.y = 8;
-			Console.WriteLine("p1 = p2. p1.x = " + p1.x + ", p1.y = " + p1.y + "; p2.x = " + p2.x + ", p2.y = " + p2.y);
+			p2.Draw();
+			List<int> numList = new List<int>();
+			numList.Add(0);
+			numList.Add(1);
+			numList.Add(2);
 
-			p1 = new Point(1, 3, '*');
-			Update(p1);
-			Console.WriteLine("Call Move. p1.x = " + p1.x + ", p1.y = " + p1.y);
+			int x = numList[0];
+			int y = numList[1];
+			int z = numList[2];
+
+			foreach (int i in numList)
+			{
+				Console.WriteLine(i);
+			}
+			numList.Remove(0);
+			List<Point> pList = new List<Point>();
+			pList.Add(p1);
+			pList.Add(p2);
+
 			Console.ReadLine();
 
-
 		}
-
-		public static void Func1(int value)
-		{
-
-		}
-
-		public static void Func2(int value)
-		{
-			value = value + 1;
-		}
-
-		public static void Func3(int x)
-		{
-			x = x + 1;
-		}
-		public static void Move(Point p, int dx, int dy)
-		{
-			p.x = p.x + dx;
-			p.y = p.y + dy;
-		}
-
-		public static void Update(Point p)
-		{
-			p = new Point();
-		}
-
-
 
 	}
 }
