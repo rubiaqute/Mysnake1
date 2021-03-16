b3bd5fcc6b7bf74c061eb2163d146e648a251fb6
rubiaqute <rubiaqute@gmail.com>
2021-03-16 22:54:00 +0300

    List homework

--- Mysnake1/Mysnake1/Program.cs
+++ Mysnake1/Mysnake1/Program.cs
@@ -13,6 +13,10 @@ namespace Mysnake1
 
 			Point p2 = new Point(4, 5, '#');
 			p2.Draw();
+			Point p3 = new Point(6, 7, '@');
+			p3.Draw();
+			Point p4 = new Point(10, 10, '%');
+			p4.Draw();
 			List<int> numList = new List<int>();
 			numList.Add(0);
 			numList.Add(1);
@@ -27,9 +31,22 @@ namespace Mysnake1
 				Console.WriteLine(i);
 			}
 			numList.Remove(0);
+
+			List<char> symList = new List<char>();
+			symList.Add('*');
+			symList.Add('&');
+			symList.Add('^');
+			symList.Add('%');
+			foreach (char i in symList)
+			{
+				Console.WriteLine(i);
+			}
+
 			List<Point> pList = new List<Point>();
 			pList.Add(p1);
 			pList.Add(p2);
+			pList.Add(p3);
+			pList.Add(p4);
 
 			Console.ReadLine();
 
