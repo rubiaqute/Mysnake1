c46c272e685eb5174e218fa21e71275a22efd0fc
rubiaqute <rubiaqute@gmail.com>
2021-03-18 14:14:44 +0300

    Змейка реагирует на нажатие клавиш

--- Mysnake1/Mysnake1/Program.cs
+++ Mysnake1/Mysnake1/Program.cs
@@ -29,22 +29,17 @@ namespace Mysnake1
 			Point p = new Point(4, 5, '*');
 			Snake snake = new Snake(p, 4, Direction.RIGHT);
 			snake.Drow();
-			snake.Move();
-			Thread.Sleep(300);
-			snake.Move();
-			Thread.Sleep(300);
-			snake.Move();
-			Thread.Sleep(300);
-			snake.Move();
-			Thread.Sleep(300);
-			snake.Move();
-			Thread.Sleep(300);
-			snake.Move();
-			Thread.Sleep(300);
-			snake.Move();
-			Thread.Sleep(300);
-			snake.Move();
-			Thread.Sleep(300);
+			
+			while(true)
+            {
+				if (Console.KeyAvailable)
+                {
+					ConsoleKeyInfo key = Console.ReadKey();
+					snake.HandleKey(key.Key);
+				}
+				Thread.Sleep(100);
+				snake.Move();
+            }
 
 
 			Console.ReadLine();
--- Mysnake1/Mysnake1/Snake.cs
+++ Mysnake1/Mysnake1/Snake.cs
@@ -42,5 +42,16 @@ namespace Mysnake1
             return nextPoint;
 
         }
+        public  void HandleKey (ConsoleKey key)
+        {
+            if (key == ConsoleKey.LeftArrow)
+                direction = Direction.LEFT;
+            else if (key == ConsoleKey.RightArrow)
+                direction = Direction.RIGHT;
+            else if (key == ConsoleKey.DownArrow)
+                direction = Direction.DOWN;
+            else if (key == ConsoleKey.UpArrow)
+                direction = Direction.UP;
+        }
     }
 }
